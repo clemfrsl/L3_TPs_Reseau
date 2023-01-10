7f335d05043e9f58c204966ee7aefa402e06a67e
f20008012 <clem.fresnel@gmail.com>
2023-01-10 14:50:53 +0100

    client

--- ClientUDP.java
+++ ClientUDP.java
@@ -4,27 +4,31 @@ import java.util.Scanner;
 
 public class ClientUDP {
     public static void main(String[] args) throws IOException {
-        Scanner scanner = new Scanner(System.in);
-        String message = scanner.next();
         int port;
         InetAddress address;
 
         if(args[1] != null && args[2] != null){
-            port = Integer.parseInt(args[1]);
-            address = InetAddress.getByName(args[2]);
+            port = Integer.parseInt(args[2]);
+            address = InetAddress.getByName(args[1]);
         }
         else{
             port = 1234;
             address = InetAddress.getByName("localhost");
         }
 
-        byte[] buffer = new byte[512];
+        Scanner scanner = new Scanner(System.in);
+        String inputMessage = scanner.next();
+
+        byte[] buffer = inputMessage.getBytes();
         DatagramSocket socket = new DatagramSocket(port, address);
 
         DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
         socket.send(packet);
+        socket.receive(packet);
+
 
 
     }
 
 }
+
