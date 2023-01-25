df0594a805dc21e7888b75baa7b7cb0cd07d84fa
d21218463 <juliette.dadoit@etu.univ-amu.fr>
2023-01-25 16:52:13 +0100

    serveur v1

--- TP3/ServerTCP.java
+++ TP3/ServerTCP.java
@@ -1,8 +1,30 @@
 package TP3;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class ServerTCP {
 
-    public static void main(String[] args){
+    public static void main(String[] args) throws IOException {
+
+        //cas UDP
+        int port = 1234;
+
+        ServerSocket serverSocket = new ServerSocket(port);
+
+        while(true){
+            Socket socket = serverSocket.accept();
+            serverSocket.close();
+            OutputStream outputStream = socket.getOutputStream();
+            outputStream.write(2);
+            InputStream inputStream = socket.getInputStream();
+            int y = inputStream.read();
+            System.out.println("> y = "+ y);
+            socket.close();
+        }
 
     }
 }
