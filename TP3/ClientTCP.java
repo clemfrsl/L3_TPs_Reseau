ac41d70c699f59a01b8692c4cb5c9a15cfd29a46
f20008012 <clem.fresnel@gmail.com>
2023-01-25 17:37:13 +0100

    client tcp boucle

--- TP3/ClientTCP.java
+++ TP3/ClientTCP.java
@@ -1,29 +1,33 @@
 package TP3;
 
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.*;
 import java.util.Scanner;
 
 public class ClientTCP {
     public static void main(String[] args) throws IOException {
-
-        Scanner scanner = new Scanner(System.in);
-
-        Socket socket = new Socket();
-        InetSocketAddress address = new InetSocketAddress(1234);
-        socket.connect(address);
-
-        while(scanner.hasNextLine()) {
-            String inputMessage = scanner.nextLine() + '\n';
-            byte[] buffer = inputMessage.getBytes();
-
-            OutputStream outputStream = socket.getOutputStream();
-            outputStream.write(buffer);
+        if(args.length != 2){
+            throw new IllegalArgumentException("Error, server or port not found.");
         }
+        else {
+            int port = Integer.parseInt(args[1]);
+            Scanner scanner = new Scanner(System.in);
+            Socket socket = new Socket();
 
-        socket.close();
+            InetSocketAddress address = new InetSocketAddress(port);
+            socket.connect(address);
+
+            while (scanner.hasNextLine()) {
+                String inputMessage = scanner.nextLine() + '\n';
+                byte[] buffer = inputMessage.getBytes();
+
+                OutputStream outputStream = socket.getOutputStream();
+                outputStream.write(buffer);
+            }
+
+            socket.close();
+        }
 
     }
 }
