55af48f3c1e16807033f5ec88e409bc929fa1467
clemf <clem.fresnel@gmail.com>
2023-01-30 21:49:15 +0100

    python

--- TP3/ClientTPC.py
+++ TP3/ClientTPC.py
@@ -1,4 +1,3 @@
-# exemple internet client TPC
 import socket
 
 address = "localhost"
@@ -8,18 +7,10 @@ client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((address, port))
 print 'Connexion vers ' + address + ':' + str(port) + ' reussie.'
 
-message = input()
-print 'Envoi de :' + message
-n = client.send(message)
-if (n != len(message)):
-        print 'Erreur envoi.'
-else:
-        print 'Envoi ok.'
-
-# supprimer si pas de donnees recues
-#print 'Reception...'
-#donnees = client.recv(1024)
-#print 'Recu :', donnees
+message = raw_input('Tapez votre message >')
+while message.lower().strip() != 'stop':
+    client.send(message+"\n".encode())
+    message = raw_input(' >')
 
 print 'Deconnexion.'
 client.close()
